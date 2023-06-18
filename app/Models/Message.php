26d719a215b0906520bee99d69f17006a5462e64
Maybeanewbie <piedraigor@gmail.com>
2023-06-18 16:21:14 -0400

    Change Message so it returns a human readable date

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class Message extends Model
 {
     use HasFactory;
+
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->locale("es")->diffForHumans();
+    }
+
 }
